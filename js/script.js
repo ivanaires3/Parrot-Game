6a44ecde7a89dad0a04e6550f2a162ee9e6dad33
Ivan Aires <ivanaires32@gmail.com>
2023-01-17 10:03:14 -0300

    imagens dos parrots

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 
+
+
 let quantidadeCartas = prompt("Com quantas cartas você deseja jogar");
 const minimo = 4;
 
@@ -22,24 +24,55 @@ let contador = 0;
 
 cartas.innerHTML = '';
 
+const imagesAleatorias = [];
+imagesAleatorias.push(`<div class="imagem-costa">
+<img src="images/bobrossparrot.gif" alt="">
+</div>`)
+imagesAleatorias.push(`<div class="imagem-costa">
+<img src="images/explodyparrot.gif" alt="">
+</div>`)
+imagesAleatorias.push(`<div class="imagem-costa">
+    <img src="images/fiestaparrot.gif" alt="">
+</div>`)
+imagesAleatorias.push(`<div class="imagem-costa">
+    <img src="images/revertitparrot.gif" alt="">
+</div>`)
+imagesAleatorias.push(`<div class="imagem-costa">
+    <img src="images/tripletsparrot.gif" alt="">
+</div>`)
+
+imagesAleatorias.push(`<div class="imagem-costa">
+    <img src="images/unicornparrot.gif" alt="">
+</div>`);
+
+imagesAleatorias.sort(comparador);
+
+function comparador() {
+    return Math.random() - 0.5;
+}
+
+
 while (contador < quantidadeCartas) {
+
+    const numeroqualquer = [0, 1, 2, 3, 4, 5]
+
+    numeroqualquer.sort(comparador);
+
+    function comparador() {
+        return Math.random() - 0.5;
+    }
 
     aleatorio.push(`<div class="carta">
     <div class="imagem-frente">
         <img src="images/back.png" alt="">
     </div>
-    </div>
-`)
+    ${imagesAleatorias[numeroqualquer[0]]}
+    
+    </div>`)
+
+
     cartas.innerHTML = cartas.innerHTML + aleatorio[contador];
 
     contador++
 }
 
-aleatorio.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
-
-// Esta função pode ficar separada do código acima, onde você preferir
-function comparador() {
-    return Math.random() - 0.5;
-}
-
-comparador();
