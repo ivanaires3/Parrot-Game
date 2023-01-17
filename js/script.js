ea402f7933a709e7ce92f11b1c76941aefd56071
Ivan Aires <ivanaires32@gmail.com>
2023-01-17 10:05:07 -0300

    Clique na carta

--- js/script.js
+++ js/script.js
@@ -1,78 +1,113 @@
-
-
-
 let quantidadeCartas = prompt("Com quantas cartas você deseja jogar");
-const minimo = 4;
 
 let numero = Number(quantidadeCartas);
 let impar = numero % 2;
 
-const cartas = document.querySelector('.cartas')
-
-
-
-while (quantidadeCartas < minimo || impar !== 0 || quantidadeCartas > 14) {
+while (quantidadeCartas < 4 || impar !== 0 || quantidadeCartas > 14) {
     alert("Número invalido, digite um número par entre 4 e 14!");
     quantidadeCartas = prompt("Com quantas cartas você deseja jogar");
     numero = Number(quantidadeCartas);
     impar = numero % 2;
 }
 
-const aleatorio = [];
+const imagesParrots = [
+    "bobrossparrot",
+    "explodyparrot",
+    "fiestaparrot",
+    "metalparrot",
+    "revertitparrot",
+    "tripletsparrot",
+    "unicornparrot",
+    "bobrossparrot",
+    "explodyparrot",
+    "fiestaparrot",
+    "metalparrot",
+    "revertitparrot",
+    "tripletsparrot",
+    "unicornparrot"
+];
 
-let contador = 0;
+const cartas = document.querySelector(".cartas");
 
-cartas.innerHTML = '';
+let card = [];
 
-const imagesAleatorias = [];
-imagesAleatorias.push(`<div class="imagem-costa">
-<img src="images/bobrossparrot.gif" alt="">
-</div>`)
-imagesAleatorias.push(`<div class="imagem-costa">
-<img src="images/explodyparrot.gif" alt="">
-</div>`)
-imagesAleatorias.push(`<div class="imagem-costa">
-    <img src="images/fiestaparrot.gif" alt="">
-</div>`)
-imagesAleatorias.push(`<div class="imagem-costa">
-    <img src="images/revertitparrot.gif" alt="">
-</div>`)
-imagesAleatorias.push(`<div class="imagem-costa">
-    <img src="images/tripletsparrot.gif" alt="">
-</div>`)
+for (let contador = 0; contador < quantidadeCartas; contador = contador + 2) {
 
-imagesAleatorias.push(`<div class="imagem-costa">
-    <img src="images/unicornparrot.gif" alt="">
-</div>`);
+    card.push(`
+    <div class='carta' data-parrot=${imagesParrots[contador]} onclick='virarCarta(this);'>
+        <div class="imagem face">
+            <img src="images/${imagesParrots[contador]}.gif" alt="">
+        </div>
+        <div class="imagem back">
+            <img src="images/back.png" alt="">
+        </div>
+    </div>`);
 
-imagesAleatorias.sort(comparador);
+    card.push(`
+    <div class='carta' data-parrot=${imagesParrots[contador]} onclick='virarCarta(this);'>
+        <div class="imagem face">
+            <img src="images/${imagesParrots[contador]}.gif" alt="">
+        </div>
+        <div class="imagem back">
+            <img src="images/back.png" alt="">
+        </div>
+    </div>`);
+
+}
+
+card.sort(comparador)
+
+for (let i = 0; i < quantidadeCartas; i++) {
+    cartas.innerHTML = cartas.innerHTML + card[i];
+}
+
+let cartaVirada1;
+let cartaVirada2;
+
+function virarCarta(cartaVirada) {
+    const esconder = cartaVirada.querySelector(".back");
+    const revelar = cartaVirada.querySelector(".face");
+
+    if (revelar.classList.contains("mostrarFace") || esconder.classList.contains("esconderCosta")) {
+        return;
+    }
+
+    if (cartaVirada1 === undefined) {
+        esconder.classList.add("esconderCosta");
+        revelar.classList.add("mostrarFace");
+        cartaVirada1 = cartaVirada;
+    } else if (cartaVirada2 === undefined) {
+        esconder.classList.add("esconderCosta");
+        revelar.classList.add("mostrarFace");
+        cartaVirada2 = cartaVirada;
+    }
+
+    setTimeout(compararCartas, 1000)
+}
+
+function compararCartas() {
+    const primeiraCarta = cartaVirada1.getAttribute("data-parrot");
+    const segundaCarta = cartaVirada2.getAttribute("data-parrot");
+
+    if (primeiraCarta === segundaCarta) {
+
+        cartaVirada1 = undefined;
+        cartaVirada2 = undefined;
+
+    } else {
+        esconder();
+        function esconder() {
+            cartaVirada1.querySelector('.mostrarFace').classList.remove('mostrarFace')
+            cartaVirada1.querySelector('.esconderCosta').classList.remove("esconderCosta")
+            cartaVirada2.querySelector('.mostrarFace').classList.remove('mostrarFace')
+            cartaVirada2.querySelector('.esconderCosta').classList.remove("esconderCosta")
+
+            cartaVirada1 = undefined;
+            cartaVirada2 = undefined;
+        }
+    }
+}
 
 function comparador() {
     return Math.random() - 0.5;
 }
-
-
-while (contador < quantidadeCartas) {
-
-    const numeroqualquer = [0, 1, 2, 3, 4, 5]
-
-    numeroqualquer.sort(comparador);
-
-    function comparador() {
-        return Math.random() - 0.5;
-    }
-
-    aleatorio.push(`<div class="carta">
-    <div class="imagem-frente">
-        <img src="images/back.png" alt="">
-    </div>
-    ${imagesAleatorias[numeroqualquer[0]]}
-    
-    </div>`)
-
-
-    cartas.innerHTML = cartas.innerHTML + aleatorio[contador];
-
-    contador++
-}
-
